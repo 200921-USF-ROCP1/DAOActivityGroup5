58ccf065ff83ce5ef75dc3d62bd144695ac14d43
emon1987 <49166522+emon1987@users.noreply.github.com>
2020-10-05 12:54:16 -0400

    Add files via upload

---
+++ MyFirstDal/src/main/java/com/revature/dao/implementations/PetsDAOGenericImpl.java
@@ -0,0 +1,35 @@
+package com.revature.dao.implementations;
+
+import java.util.List;
+
+import com.revature.dao.interfaces.GenericDAO;
+import com.revature.models.Pets;
+
+public class PetsDAOGenericImpl implements GenericDAO<Pets> {
+
+	public void create(Pets t) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public Pets get(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void update(Pets t) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void delete(Pets t) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public List<Pets> getAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
